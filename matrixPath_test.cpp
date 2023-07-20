8635f36e8e37fabab8a8158f2de59cddda3af9ee
FusionPower <salas._@hotmail.com>
2023-07-20 10:17:27 -0400

    fix 6CDCI

--- matrixPath_test.cpp
+++ matrixPath_test.cpp
@@ -24,6 +24,27 @@ TEST(smallestSumTest, HandlesThreeByThreeMatrix) {
   EXPECT_EQ(result.first, 7);
 }
 
+// Test with doubles.
+TEST(smallestSumTest, HandlesDouble) {
+  std::vector<std::vector<double>> matrix = {{1.1, 3.1, 1.1}, {1.1, 5.1, 1.1}, {4.1, 2.1, 1.1}};
+  auto result = smallestSum<double>(matrix);
+  EXPECT_EQ(result.first, 7.7);
+}
+
+// Test with floats.
+TEST(smallestSumTest, HandlesFloat) {
+  std::vector<std::vector<float>> matrix = {{1.1, 3.1, 1.1}, {1.1, 5.1, 1.1}, {4.1, 2.1, 1.1}};
+  auto result = smallestSum<float>(matrix);
+  EXPECT_EQ(result.first, 7.7);
+}
+
+// Test with long.
+TEST(smallestSumTest, HandlesLong) {
+  std::vector<std::vector<long>> matrix = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+  auto result = smallestSum<long>(matrix);
+  EXPECT_EQ(result.first, 7);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
