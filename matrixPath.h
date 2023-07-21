cc7b7f00bb2401acdf7d2a40c5ad3f6df4ebdb67
FusionPower <salas._@hotmail.com>
2023-07-21 09:13:20 -0400

    improved modularization

--- matrixPath.h
+++ matrixPath.h
@@ -11,15 +11,11 @@ Assumptions:
 */
 
 template <typename T>
-// Returns minimum path sum and shortest path
-std::pair <T, std::vector<std::pair<int, int>>> findMinPath(std::vector<std::vector<T>>& matrix) {
-    if (matrix.empty() || matrix.size() != matrix[0].size()) {
-        throw std::invalid_argument("Matrix must be non-empty and square.");
-    }
-    
-    int n = matrix.size();
-	
-	std::vector<std::pair<int, int>> path;
+void preprocessMatrix(std::vector<std::vector<T>>& matrix, int n) {
+    /*
+    find the best element between i-1,j and i,j-1 to add to the current element to
+    for every element in the matrix, except the first row and column.
+    */
     for(int i=1; i<n; i++){
         if (matrix[i][0]<0){
             throw std::invalid_argument("Found negative element in position (" + std::to_string(i) + ", " + std::to_string(0) + ").");
@@ -42,6 +38,14 @@ std::pair <T, std::vector<std::pair<int, int>>> findMinPath(std::vector<std::vec
             }
         }
     }
+}
+
+template <typename T>
+std::vector<std::pair<int, int>> backtrackPath(std::vector<std::vector<T>>& matrix, int n){
+    /*
+    backtrack from the bottom right corner to the top left corner to find the path
+    */
+	std::vector<std::pair<int, int>> path;
     int i = n-1;
 	int j = n-1;
 	while (~i!=0 && ~j!=0) {
@@ -56,6 +60,21 @@ std::pair <T, std::vector<std::pair<int, int>>> findMinPath(std::vector<std::vec
 
 	}
 	std::reverse(path.begin(), path.end());
+    return path;
+    }
+
+template <typename T>
+std::pair <T, std::vector<std::pair<int, int>>> findMinPath(std::vector<std::vector<T>>& matrix) {
+    /*
+    Returns minimum path sum and shortest path
+    */ 
+    if (matrix.empty() || matrix.size() != matrix[0].size()) {
+        throw std::invalid_argument("Matrix must be non-empty and square.");
+    }
+    int n = matrix.size();
+    
+    preprocessMatrix(matrix, n);
+    std::vector<std::pair<int, int>> path = backtrackPath(matrix, n);
     return {std::abs(matrix[n-1][n-1]), path};
 }
 
